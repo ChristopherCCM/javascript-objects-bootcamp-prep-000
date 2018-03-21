4fe499b59bc80bd38b52f8c2c513e5acfbd8b55f
Christopher <christopherccm@msn.com>
2018-03-21 20:09:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(obj, key, value) {
   return obj
 }
 
-function removeFromPlaylist(obj, key) {
+function removeFromPlaylist(playlist, artistName) {
   delete obj.key;
   return obj
 }
